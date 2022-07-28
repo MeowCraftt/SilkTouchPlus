494e92b13fa3714f97a658111c18334666a17e65
MeowCraft_ <109906956+MeowCraftt@users.noreply.github.com>
2022-07-28 11:44:09 -0400

    Create SilkTouchPlus.java
    unfinished

---
+++ SilkTouchPlus.java
@@ -0,0 +1,13 @@
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SilkTouchPlus extends JavaPlugin implements Listener {
+  
